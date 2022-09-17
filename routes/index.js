fc77e0467bbd2e3f5a70902bf7f892cd0cff9226
Wilber Paredes <wilberparedes@gmail.com>
2022-09-17 02:00:03 -0500

    refactor: versionado of endpoints

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,15 @@
 /** archivo que configura las rutas */
+const express = require('express');
 const productsRouter = require('./products.router');
 const usersRouter = require('./users.router');
 const categoriesRouter = require('./categories.router');
 
 const routerApi = (app) => {
-  app.use('/products', productsRouter);
-  app.use('/users', usersRouter);
-  app.use('/categories', categoriesRouter);
+  const router = express.Router();
+  app.use('/api/v1/', router);
+  router.use('/products', productsRouter);
+  router.use('/users', usersRouter);
+  router.use(' /categories', categoriesRouter);
 };
 
 module.exports = routerApi;
